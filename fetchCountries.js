e9922a602a68dc42c1f7c7a323d710e7ae78f5f3
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-03-26 19:49:21 +0200

    fetchCountries

---
+++ fetchCountries.js
@@ -0,0 +1,11 @@
+export const fetchCounries = countryName => {
+  const BASE_URL = 'https://restcountries.com/v3.1/name/';
+  const SEARCH_PARAMS = '?fields=name,capital,population,flags,languages';
+
+  return fetch(`${BASE_URL}${countryName}${SEARCH_PARAMS}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCounries } from './fetchCountries';
+import counrtyCardTemp from '../country-card.hbs';
+import counrtyCardTempList from '../country-card-list.hbs';
 
 const DEBOUNCE_DELAY = 300;
