47e79f259957092c0254a17d2b14455fade1a9c5
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-03-26 20:17:33 +0200

    a

--- fetchCountries.js
+++ fetchCountries.js
@@ -1,11 +1,26 @@
-export const fetchCounries = countryName => {
-  const BASE_URL = 'https://restcountries.com/v3.1/name/';
-  const SEARCH_PARAMS = '?fields=name,capital,population,flags,languages';
+// export const fetchCounries = countryName => {
+//   const BASE_URL = 'https://restcountries.com/v3.1/name/';
+//   const SEARCH_PARAMS = '?fields=name,capital,population,flags,languages';
 
-  return fetch(`${BASE_URL}${countryName}${SEARCH_PARAMS}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
+//   return fetch(`${BASE_URL}${countryName}${SEARCH_PARAMS}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// };
+
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+
+const fetchCountries = name => {
+  return fetch(
+    `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (response.status === 404) {
+      return Promise.reject(new Error());
     }
     return response.json();
   });
 };
+
+export { fetchCountries };
